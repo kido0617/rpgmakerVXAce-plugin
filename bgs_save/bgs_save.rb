59cd39661fa2f78ecc6475629656b9cf323589bc
kido0617 <enokido0617@gmail.com>
2018-08-04 16:34:35 +0900

    update

---
+++ bgs_save/bgs_save.rb
@@ -0,0 +1,28 @@
+#===============================================================================
+# 2018/08/05 kido0617
+# http://kido0617.github.io/
+# 完全に自由にどうぞ。
+# クレジットの表記もいりません。
+# スクリプトコマンドで以下のように実行します
+# BGSの保存: $game_system.save_bgs
+# BGSの再開' $game_system.replay_bgs
+#
+# Ver.1.0
+#-------------------------------------------------------------------------------
+
+class Game_System
+
+  alias myinit initialize
+  def initialize
+    myinit
+    @saved_bgs = nil
+  end
+ 
+  def save_bgs
+    @saved_bgs = RPG::BGS.last
+  end
+
+  def replay_bgs
+    @saved_bgs.replay if @saved_bgs
+  end
+end
